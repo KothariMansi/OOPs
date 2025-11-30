ce77a7431e02d4c425627f138fe4a0ee11fd3c7f
SHREE <mansikothari46@gmail.com>
2025-11-30 21:25:18 +0530

    Inheritance using Shape class example

--- OOPs.java
+++ OOPs.java
@@ -1,28 +1,46 @@
-// Polymorphism
-// Function overloading
-// Compile time polymorphism
-class Student {
-    String name;
-    int age;
+// Inheritance
 
-    public void printInfo(String name) {
-        System.out.println(name);
+// Base Class
+class Shape {
+    public void area() {
+        System.out.println("Display Area");
+    }
+}
 
+// Sub Class
+class Triangle extends Shape {
+    public void area(int b, int h) {
+        System.out.println(1/2 * b * h);
     }
-    public void printInfo(int age) {
-        System.out.println(age);
+}
+
+// Sub Class
+class EquilateralTraingle extends Triangle {
+    public void area(int b, int h) {
+        System.out.println(1/2 * b * h);
     }
-    public void printInfo(String name, int age) {
-        System.out.println(name + " " + age);
+}
+
+// Sub Class
+class Circle extends Shape {
+    public void area(int r) {
+        System.out.println(3.14 * r * r);
     }
 }
 
 public class OOPs{
     public static void main(String args[]) {
-        Student student1 = new Student();
-        student1.name = "Ram";
-        student1.age = 12;
-        student1.printInfo(student1.name);
-        student1.printInfo(student1.name, student1.age);
+        Triangle t1 = new Triangle();
+        t1.color = "Red";
     }
 }
+
+// Inheritance Type
+// 1. Single Level Inheritance = Base Class -> Derived Class
+// 2. Multi Level Inheritance = Base Class -> Derived Class -> Derived Class
+// 3. Hierarchial Inheritance = Derived Class <- Base Class -> Derived Class
+// 4. Hybrid Inheritance =  Base Class
+    //                      /        \
+    //          Dervide Class       Derived Class
+    //                |
+    //          Derived Class
