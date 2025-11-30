db03e630a349fe673ee111328cacb9ae2506d92b
SHREE <mansikothari46@gmail.com>
2025-11-30 21:11:38 +0530

    Refactor Student class to demonstrate polymorphism and function overloading

--- OOPs.java
+++ OOPs.java
@@ -1,51 +1,28 @@
-class Pen {
-    String color;
-    String type;
-
-    public void write(){
-        System.out.println("Something is writing...");
-    }
-
-    public void printColor() {
-        System.out.println(this.color);
-    }
-}
-
+// Polymorphism
+// Function overloading
+// Compile time polymorphism
 class Student {
     String name;
     int age;
 
-    public void printInfo() {
-        System.out.println(this.name);
-        System.out.println(this.age);
-    }
-    Student(Student s2) {
-        this.name = s2.name;
-        this.age = s2.age;
-    }
+    public void printInfo(String name) {
+        System.out.println(name);
 
-    Student() {}
+    }
+    public void printInfo(int age) {
+        System.out.println(age);
+    }
+    public void printInfo(String name, int age) {
+        System.out.println(name + " " + age);
+    }
 }
 
 public class OOPs{
     public static void main(String args[]) {
-        Pen pen1 = new Pen();
-        pen1.color = "Blue";
-        pen1.type = "Ball Pen";
-        pen1.write();
-
-        Pen pen2 = new Pen();
-        pen2.color = "Black";
-        pen2.type = "Gel Pen";
-        pen1.printColor();
-        pen2.printColor();
-
         Student student1 = new Student();
         student1.name = "Ram";
         student1.age = 12;
-
-        Student student2 = new Student(student1);
-        student2.printInfo();
-
+        student1.printInfo(student1.name);
+        student1.printInfo(student1.name, student1.age);
     }
 }
