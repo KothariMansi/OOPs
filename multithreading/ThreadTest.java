7aba23f8c720cc64c2633222a9ef21bcb5273615
SHREE <mansikothari46@gmail.com>
2026-01-17 13:58:26 +0530

    Implement Stack class to undesrtand synchronization

--- multithreading/ThreadTest.java
+++ multithreading/ThreadTest.java
@@ -3,13 +3,19 @@ package multithreading;
 public class ThreadTest {
     public static void main(String[] args) {
         System.out.println("main is starting");
-        Thread1 thread1 = new Thread1("thread1");
-      //  thread1.setDaemon(true);
-        thread1.start();
+        Stack stack = new Stack(5);
+        new Thread(() -> {
+            int counter = 0;
+            while(++ counter < 10) {
+                System.out.println("Pushed: " + stack.push(100));
+            }
+        }, "Pusher").start();
 
-        Thread thread2 = new Thread(new Thread2(), "thread2");
-        thread2.start();
+        new Thread(() -> {
+            int counter = 0;
+            while(++ counter < 10) 
+                System.out.println("Popped: " + stack.pop());
+        }, "Popper").start();
         System.out.println("main is ending");
-
     }
 }
