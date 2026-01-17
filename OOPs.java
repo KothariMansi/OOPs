ff0ca6fc6d269974f81c5d05dd0934ef33391e20
SHREE <mansikothari46@gmail.com>
2026-01-17 12:30:05 +0530

    Refactor Animal class to use interface

--- OOPs.java
+++ OOPs.java
@@ -1,34 +1,18 @@
-abstract class Animal {
-    Animal() {
-        System.out.println("You are creatig a new Animal....");
-    }
-    abstract void walk();
-    public void eats() {
-        System.out.println("Animal eats");
-    }
+interface Animal {
+    public void walks();
+
 }
 
-class Horse extends Animal {
-    Horse() {
-        System.out.println("Horse Created.");
-    }
-    public void walk() {
-        System.out.println("Walks on 4 legs");
-    }
-}
-
-class Chicken extends Animal {
-    public void walk() {
-        System.out.println("Walks on 2 legs");
+class Horse implements Animal {
+    public void walks() {
+        System.out.println("Walks on 4 legs.");
     }
 }
 
 public class OOPs{
     public static void main(String args[]) {
-        Horse horse1 = new Horse();
-        horse1.walk();
-        horse1.eats();
-
+        Horse horse = new Horse();
+        horse.walks();
     }
 }
 
